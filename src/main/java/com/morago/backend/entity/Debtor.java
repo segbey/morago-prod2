c248a3451bb016b9fed4fbe8ebc6a01fc3d876e3
demi <nurlankyzy.972@gmail.com>
2025-08-15 00:05:20 +0900

    commit "uploading entities"

---
+++ src/main/java/com/morago/backend/entity/Debtor.java
@@ -0,0 +1,54 @@
+package com.morago.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "debtors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Debtor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    //@Column(name = "user_id", length = 50)
+    //private String userId;
+
+    @Column(name = "account_holder", length = 200)
+    private String accountHolder;
+
+    @Column(name = "name_of_bank", length = 200)
+    private String nameOfBank;
+
+    @Column(name = "is_paid")
+    private Boolean isPaid;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+}
